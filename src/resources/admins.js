cf71f02854984de00971c222071ced70b5e4d9d7
AleMac17 <alemacia17@gmail.com>
2023-05-05 21:14:35 -0300

    MR: added get and getById admin feature fixed

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -5,17 +5,23 @@ const admins = require('../data/admins.json');
 const router = express.Router();
 
 router.get('/get', (req, res) => {
-  res.send(admins);
+  if (!admins) {
+    res.send('There are not Admins');
+  } else {
+    res.send(admins);
+  }
 });
 
 router.get('/getById/:id', (req, res) => {
   const adminId = req.params.id;
+  if (!adminId) {
+    return res.send('Please enter a valid id');
+  }
   const foundAdmin = admins.find((admin) => admin.id.toString() === adminId);
   if (foundAdmin) {
-    res.send(foundAdmin);
-  } else {
-    res.send('Admin not found!');
+    return res.send(foundAdmin);
   }
+  return res.send('Admin not found!');
 });
 
 module.exports = router;
