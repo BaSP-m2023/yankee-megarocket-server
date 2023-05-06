1585409b2bad79f039e749a4f745e3181bf20694
Juan Sobral <JuanignacioSobral@gmail.com>
2023-05-06 11:28:48 -0300

    MR get-user-by-id

--- src/resources/member.js
+++ src/resources/member.js
@@ -6,12 +6,26 @@ const router = express.Router();
 const members = require('../data/member.json');
 
 router.get('/get', (req, res) => {
-  if (!members) {
+  if (members.length === 0) {
     res.send('oops! There are no members!');
   } else {
     res.send(members);
   }
 });
 
-module.exports = router;
 // FIN DEL GET
+
+// GET BY ID
+
+router.get('/getById/:id', (req, res) => {
+  const memberId = req.params.id;
+  const foundMember = members.find((member) => member.id.toString() === memberId);
+  if (foundMember) {
+    res.send(foundMember);
+  } else {
+    res.send('Member not found!');
+  }
+});
+
+module.exports = router;
+// FIN DEL GET BY ID
