86be40d86b7df3b85f547d94b34016990bc6af2f
Nicolas Blasco <nicolas.blasco@radiumrocket.com>
2023-05-07 13:31:27 -0300

    BaSP-MR: Fix trainers router in index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,20 @@
-// use "import" to import libraries
 import express from 'express';
 import cors from 'cors';
 
-// use "require" to import JSON files
-const admins = require('./data/admins.json');
 const adminRouter = require('./resources/admins');
+const trainerRouter = require('./resources/trainer');
 
 const app = express();
 const port = process.env.PORT || 4000;
-
-const trainerRouter = require('./resources/trainer');
-
-app.use('/', trainerRouter);
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/admins', adminRouter);
+app.use('/trainers', trainerRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
 });
 
 app.listen(port, () => {
