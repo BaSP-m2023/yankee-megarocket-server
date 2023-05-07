1aa22dd02ffc3f016b9b2fb6cc5934c8758107c8
Ticiano Licarzze <ticilicarzze@gmail.com>
2023-05-07 20:01:36 -0300

    MR: correct PR to optimize the code

--- src/resources/class.js
+++ src/resources/class.js
@@ -1,24 +1,21 @@
 const express = require('express');
 
 const router = express.Router();
-const classes = require('../data/class.json');
+const Classes = require('../data/class.json');
 
 router.get('/', (req, res) => {
-  if (classes.length < 1) {
+  if (Classes.length < 1) {
     res.status(404).send('No classes available');
   } else {
-    res.send(classes);
+    res.send(Classes);
   }
 });
 
 router.get('/:id', (req, res) => {
   const classId = req.params.id;
-  const foundClass = classes.find((classes) => classes.id.toString() === classId);
-  if(foundClass){
-    res.send(foundClass);
-  } else{
-    res.status(404).send('Class not found')
-  }
+  const foundClass = Classes.find((classes) => classes.id.toString() === classId);
+  if (!foundClass) return res.status(404).send('Class not found!');
+  return res.send(foundClass);
 });
 
 module.exports = router;
