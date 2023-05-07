1125e7e75e2eaf861c4334893252c284de1060d6
AraEfe <ara.fnz@live.com.ar>
2023-05-06 21:05:37 -0300

    MR: Updated changes to trainer.js

--- src/resources/trainer.js
+++ src/resources/trainer.js
@@ -1,17 +1,20 @@
-// Get & getById Trainer → Araceli
 const express = require('express');
 
 const trainers = require('../data/trainer.json');
 
 const router = express.Router();
 
-router.get('/get', (req, res) => {
-  res.send(trainers);
+router.get('/', (req, res) => {
+  if (trainers.length === 0) {
+    res.status(404).send('No trainers available at the moment');
+  } else {
+    res.send(trainers);
+  }
 });
 
-router.get('/get/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
-  const trainer = trainers.find((t) => t.id === id);
+  const trainer = trainers.find((trainerData) => trainerData.id === id);
 
   if (trainer) {
     res.send(trainer);
