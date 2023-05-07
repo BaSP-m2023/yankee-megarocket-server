26e54a8de2a36705c76178e748de64c342dd2868
Franco rossi <rossifranco748@gmail.com>
2023-05-07 14:39:16 -0300

    MR: bug fixes

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const fs = require('fs/promises');
+const fs = require('fs');
 const admins = require('../data/admins.json');
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
+router.post('/', async (req, res) => {
   const {
     id, firstName, lastName, dni, phone, email, password,
   } = req.body;
@@ -13,22 +13,12 @@ router.post('/register', async (req, res) => {
     res.status(400).json({ error: 'All fields must be completed' });
     return;
   }
-  const newAdmin = {
-    id,
-    firstName,
-    lastName,
-    dni,
-    phone,
-    email,
-    password,
-  };
   try {
-    admins.push(newAdmin);
+    admins.push(req.body);
     await fs.writeFile('src/data/admins.json', JSON.stringify(admins, null, 2));
     res.send('Admin successfully created');
   } catch (err) {
     res.status(500).json({ error: 'Error! new admin could not be created' });
   }
 });
-
 module.exports = router;
