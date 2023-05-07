016e13839cb24f9c0a5d5094135bbe3f8b4e2031
Roberto-Orazi <robervgg@gmail.com>
2023-05-07 16:56:01 -0300

    MR: Corrections

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import express from 'express';
 import cors from 'cors';
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
-
 const superAdminRouter = require('./resources/super-admins').default;
 
 const subscriptionRouter = require('./resources/subscription');
@@ -15,7 +13,7 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/', superAdminRouter);
+app.use('/super-admins', superAdminRouter);
 app.use('/subscriptions', subscriptionRouter);
 
 app.use('/admins', adminRouter);
@@ -24,12 +22,6 @@ app.use('/trainers', trainerRouter);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
-});
-
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
