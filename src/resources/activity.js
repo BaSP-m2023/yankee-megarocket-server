fbc557f3cd6c17257224639e0b43e0e03e61b14a
JuanManuel-Blois <juan.m.blois@gmail.com>
2023-05-08 14:46:31 -0300

    MR added validation to activity and removed extra delete work on super-admin

--- src/resources/activity.js
+++ src/resources/activity.js
@@ -6,14 +6,15 @@ const activities = require('../data/activity.json');
 
 router.get('/', (req, res) => {
   if (activities.length < 1) {
-    res.status(404).send('No registerd activities');
+    res.status(404).send('No registered activities');
   } else { res.send(activities); }
 });
 
 router.get('/:id', (req, res) => {
   const activityID = req.params.id;
   const foundActivity = activities.find((activity) => activity.id.toString() === activityID);
-  res.send(foundActivity);
+  if (!foundActivity) return res.send('Activity not found');
+  return res.send(foundActivity);
 });
 
 module.exports = router;
--- src/resources/super-admins.js
+++ src/resources/super-admins.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 import { writeFile } from 'fs/promises';
 import superAdmins from '../data/super-admins.json';
 
-const fs = require('fs');
-
 const router = Router();
 router.post('/', async (req, res) => {
   const { id, email, password } = req.body;
@@ -20,18 +18,6 @@ router.post('/', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'Error, Super admin user could not be created' });
   }
-});
-
-router.delete('/:id', (req, res) => {
-  const superId = req.params.id;
-  const filteredSuperAdmin = superAdmins.filter((SA) => SA.id.toString() !== superId);
-  fs.writeFile('src/data/super-admins.json', JSON.stringify(filteredSuperAdmin, null, 2), (err) => {
-    if (err) {
-      res.send('Super Admin could not be deleted');
-    } else {
-      res.send('Super Admin has been removed');
-    }
-  });
 });
 
 export default router;
