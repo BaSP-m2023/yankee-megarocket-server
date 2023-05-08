3a15dde9bd6bf8741f39899951feb9e4ba5b50c4
Matias Sauvanet <matiassauvanet@gmail.com>
2023-05-08 01:48:58 -0300

    BaSP-MR: Create subscription

--- src/resources/subscription.js
+++ src/resources/subscription.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
-import { promises as fs } from 'fs';
+// import { promises as fs } from 'fs';
 
 const router = Router();
+const subscription = require('../data/subscription.json');
+const fs = require('fs');
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
@@ -26,6 +28,25 @@ router.delete('/:id', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+});
+
+router.post('/', async (req, res) => {
+  const {
+    id, classId, memberId, date,
+  } = req.body;
+
+  if (!id || !classId || !memberId || !date) {
+    res.status(400).json({ error: 'Complete fields' });
+    return;
+  }
+  subscription.push(req.body);
+  fs.writeFile('src/data/subscription.json', JSON.stringify(subscription), (err) => {
+    if (err) {
+      res.send('Error! Subscription cannot be created.');
+    } else {
+      res.send('Subscription Created!');
+    }
+  });
 });
 
 export default router;
