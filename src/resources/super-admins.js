0b24bb987c20e9cb2c0091758e0a30d86497af95
JuanManuel-Blois <juan.m.blois@gmail.com>
2023-05-08 01:51:52 -0300

    MR: created delete feature for super admin

--- src/resources/super-admins.js
+++ src/resources/super-admins.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { writeFile } from 'fs/promises';
 import superAdmins from '../data/super-admins.json';
 
+const fs = require('fs');
+
 const router = Router();
 router.post('/', async (req, res) => {
   const { id, email, password } = req.body;
@@ -19,4 +21,17 @@ router.post('/', async (req, res) => {
     res.status(500).json({ error: 'Error, Super admin user could not be created' });
   }
 });
+
+router.delete('/:id', (req, res) => {
+  const superId = req.params.id;
+  const filteredSuperAdmin = superAdmins.filter((SA) => SA.id.toString() !== superId);
+  fs.writeFile('src/data/super-admins.json', JSON.stringify(filteredSuperAdmin, null, 2), (err) => {
+    if (err) {
+      res.send('Super Admin could not be deleted');
+    } else {
+      res.send('Super Admin has been removed');
+    }
+  });
+});
+
 export default router;
