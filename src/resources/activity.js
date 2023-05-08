c162f866f5390c845aa4e2822a2c68a41c0c54a5
JuanManuel-Blois <juan.m.blois@gmail.com>
2023-05-08 01:58:24 -0300

    MR: adde get and get by ID features on activities

---
+++ src/resources/activity.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+const activities = require('../data/activity.json');
+
+router.get('/', (req, res) => {
+  if (activities.length < 1) {
+    res.status(404).send('No registerd activities');
+  } else { res.send(activities); }
+});
+
+router.get('/:id', (req, res) => {
+  const activityID = req.params.id;
+  const foundActivity = activities.find((activity) => activity.id.toString() === activityID);
+  res.send(foundActivity);
+});
+
+module.exports = router;
