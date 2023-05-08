e5accad9a997439c147510170ec8ff72c68fa462
AleMac17 <alemacia17@gmail.com>
2023-05-08 15:59:49 -0300

    MR: post feature for activity

--- src/resources/activity.js
+++ src/resources/activity.js
@@ -6,21 +6,28 @@ const router = express.Router();
 const activities = require('../data/activity.json');
 
 router.post('/', (req, res) => {
-  const newActivity = req.body;
-  if (Object.keys(newActivity).length === 3
-  && Object.values(newActivity).length === 3
-  && Object.values(newActivity).indexOf({}) >= 0) {
-    activities.push(newActivity);
-    fs.writeFile('src/data/activity.json', JSON.stringify(activities, null, 2), (err) => {
-      if (err) {
-        res.send('Error! Could not create an activity!');
-      } else {
-        res.send('Activity Created!');
-      }
-    });
-  } else {
-    res.send('Error. The activity must contain exactly 3 items with some value each.');
+  const {
+    id, name, description,
+  } = req.body;
+
+  if (!id || !name || !description) {
+    res.status(400).json({ error: 'All fields must be completed' });
+    return;
   }
+  const activityExists = activities.find((activityData) => activityData.id === id);
+
+  if (activityExists) {
+    res.status(400).json({ error: 'This ID already exists' });
+    return;
+  }
+  activities.push(req.body);
+  fs.writeFile('src/data/activity.json', JSON.stringify(activities), (err) => {
+    if (err) {
+      res.status(500).json({ error: 'Error! Activity could not be created' });
+    } else {
+      res.send('Activity successfully created');
+    }
+  });
 });
 
 module.exports = router;
