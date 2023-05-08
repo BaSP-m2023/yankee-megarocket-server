97c491ee204b361652fda95a270ef5476b18a0df
jramire5 <jjnicoramirez@gmail.com>
2023-05-08 15:40:23 -0300

    MR: member: fixed index comments

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 
-// use "require" to import JSON files
-// import superAdminRouter from './resources/super-admins';
-/// import subscriptionRouter from './resources/subscription';
+import superAdminRouter from './resources/super-admins';
+import subscriptionRouter from './resources/subscription';
 
 const memberRouter = require('./resources/member');
-// const adminRouter = require('./resources/admins');
-// const trainerRouter = require('./resources/trainer');
-// const classRouter = require('./resources/class');
+const adminRouter = require('./resources/admins');
+const trainerRouter = require('./resources/trainer');
+const classRouter = require('./resources/class');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -16,11 +15,11 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-// app.use('/super-admins', superAdminRouter);
-// app.use('/subscriptions', subscriptionRouter);
-// app.use('/admins', adminRouter);
-// app.use('/trainers', trainerRouter);
-// app.use('/classes', classRouter);
+app.use('/super-admins', superAdminRouter);
+app.use('/subscriptions', subscriptionRouter);
+app.use('/admins', adminRouter);
+app.use('/trainers', trainerRouter);
+app.use('/classes', classRouter);
 app.use('/members', memberRouter);
 
 app.get('/', (req, res) => {
