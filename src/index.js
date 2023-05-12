d6dc2e1154a373b8a10fe97586c23b8d67987e85
AleMac17 <alemacia17@gmail.com>
2023-05-12 13:31:24 -0300

    MR-1: Connect MongoDB, separate index.js into app.js && folders fixes

--- src/index.js
+++ src/index.js
@@ -1,34 +1,22 @@
-import express from 'express';
-import cors from 'cors';
+import mongoose from 'mongoose';
+import app from './app';
 
-import superAdminRouter from './resources/super-admins';
-
-const memberRouter = require('./resources/member');
-const adminRouter = require('./resources/admins');
-const trainerRouter = require('./resources/trainer');
-const classRouter = require('./resources/class');
-const activityRouter = require('./resources/activity');
-const subscriptionRouter = require('./resources/subscription');
-
-const app = express();
 const port = process.env.PORT || 4000;
+const DB_URL = 'mongodb+srv://yankee-team:4csuRIPMo2frFlCj@megarocket-databases.inpprte.mongodb.net/yankee-database';
 
-app.use(cors());
-app.use(express.json());
-
-app.use('/super-admins', superAdminRouter);
-app.use('/subscriptions', subscriptionRouter);
-app.use('/admins', adminRouter);
-app.use('/trainers', trainerRouter);
-app.use('/classes', classRouter);
-app.use('/members', memberRouter);
-app.use('/activities', activityRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Example app listening on port ${port}`);
-});
+mongoose.connect(
+  DB_URL,
+  (error) => {
+    if (error) {
+      // eslint-disable-next-line no-console
+      console.log('Failed connection to database', error);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log('Connected to database');
+      app.listen(port, () => {
+        // eslint-disable-next-line no-console
+        console.log(`Server ready on port ${port}`);
+      });
+    }
+  },
+);
