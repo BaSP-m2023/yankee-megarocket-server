3b6e5148ce68badda23787fc58e58a76ff992b77
Franco rossi <rossifranco748@gmail.com>
2023-05-13 20:03:36 -0300

    MR-2:create the admin schema

---
+++ src/models/Admin.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const adminSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      maxlength: 10,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      maxlength: 10,
+    },
+    dni: {
+      type: Number,
+      required: true,
+      min: 7,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model('Admin', adminSchema);
