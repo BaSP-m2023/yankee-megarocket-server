2654b63bc6795c9ddc24c3bb7b03aba2c1832425
AraEfe <ara.fnz@live.com.ar>
2023-05-15 13:01:23 -0300

    MR-6: Removed unused  variable on validations, exported postman endpoints

--- src/validations/trainers.js
+++ src/validations/trainers.js
@@ -10,12 +10,12 @@ const validateTrainer = (req, res, next) => {
       .required(),
     password: Joi.string().min(6).required(),
     rate: Joi.number().min(10).max(15).required(),
-    assignedActivities: Joi.array().items(Joi.string()).required(),
+    assignedActivities: Joi.array().items(Joi.string()),
   });
   const trainerData = dataValidation.validate(req.body);
-  if (!dataValidation.error) return next();
+  if (!trainerData.error) return next();
   return res.status(400).json({
-    message: `There was an error: ${trainerData.error.details[0].message}`,
+    message: `There was an error: ${trainerData.error.message}`,
     data: undefined,
     error: true,
   });
