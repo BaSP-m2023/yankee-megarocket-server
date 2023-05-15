880882c23852e25c393a4865769bc0bfce4415db
JuanManuel-Blois <juan.m.blois@gmail.com>
2023-05-15 18:17:27 -0300

    MR-12: added activities validations and postamn test

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { createActivity, getActivity, getActivityId } from '../controllers/activities';
+import { activityCreateValidation } from '../validations/activities';
 
 const router = express.Router();
 router.get('/', getActivity);
 router.get('/:id', getActivityId);
-router.post('/', createActivity);
+router.post('/', activityCreateValidation, createActivity);
 
 export default router;
