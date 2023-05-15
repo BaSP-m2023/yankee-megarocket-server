e07fe034da0e3baba9545f6d6c41f533ade58a6a
jramire5 <jjnicoramirez@gmail.com>
2023-05-15 11:07:33 -0300

    MR-5: PUT: Controller and validations updated, Delete: Controller updated

--- src/controllers/members.js
+++ src/controllers/members.js
@@ -1,69 +1,30 @@
 import Member from '../models/Member';
 
-export const getMember = async (req, res) => {
-  try {
-    const member = await Member.find();
-    return res.status(201).json({
-      message: 'This is a list of our members:',
-      data: member,
-      error: false,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      message: error, // error.toString()
-      data: undefined,
-      error: true,
-    });
-  }
-};
-export const getMemberId = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const foundMember = await Member.find((member) => member.id === id);
-    return res.send(foundMember);
-  } catch (error) {
-    return res.send(error);
-  }
-};
-export const createMember = async (req, res) => {
-  try {
-    const {
-      firstName, lastName, dni, email, phone, password,
-    } = req.body;
-    await Member.create({
-      firstName,
-      lastName,
-      dni,
-      email,
-      phone,
-      password,
-    });
-    return res.status(201).json({
-      message: 'Member created',
-      data: Member,
-      error: false,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      message: error, // error.toString()
-      data: undefined,
-      error: true,
-    });
-  }
-};
 export const editMember = async (req, res) => {
   try {
     const {
       _id, firstName, lastName, dni, email, phone, password,
     } = req.body;
-    await Member.findOneAndUpdate({ _id }, {
-      firstName,
-      lastName,
-      dni,
-      email,
-      phone,
-      password,
-    });
+    const memberFound = await Member.findById(_id);
+    if (!memberFound) {
+      return res.status(404).json({
+        message: 'Member Updated',
+        data: _id,
+        error: true,
+      });
+    }
+    await Member.findOneAndUpdate(
+      { _id },
+      {
+        firstName,
+        lastName,
+        dni,
+        email,
+        phone,
+        password,
+      },
+      { new: true },
+    );
     return res.status(201).json({
       message: 'Member Updated',
       data: {
@@ -77,22 +38,25 @@ export const editMember = async (req, res) => {
       error: false,
     });
   } catch (error) {
-    return res.status(500).send(error);
+    return res.status(500).json({
+      message: 'Server Erorr',
+      data: undefined,
+      error: true,
+    });
   }
 };
 export const deleteMember = async (req, res) => {
   try {
     const { id } = req.params;
     const foundMember = await Member.findById(id);
-    await Member.findByIdAndDelete(id);
-
     if (!foundMember) {
       return res.status(404).json({
-        message: 'Id not found',
-        data: undefined,
+        message: `${id} This id was not found`,
+        data: {},
         error: true,
       });
     }
+    await Member.findByIdAndDelete(id);
     return res.status(201).json({
       message: 'Member Deleted',
       data: foundMember,
