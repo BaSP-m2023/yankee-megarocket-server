cd71c7283aecbfd7f0b04211bb79bb361c622097
AraEfe <ara.fnz@live.com.ar>
2023-05-15 18:48:48 -0300

    MR-14: created validations for post, and routes connections

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,12 @@ import trainerRoutes from './trainers';
 
 import membersRoutes from './members';
 
+import superAdminsRoutes from './super-admins';
+
 const router = express.Router();
 
 router.use('/trainers', trainerRoutes);
 router.use('/members', membersRoutes);
+router.use('/super-admins', superAdminsRoutes);
 
 export default router;
