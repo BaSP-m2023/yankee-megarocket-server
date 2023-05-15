fcdc6333fb4f18d80742cb35a27eeb7703be5a73
AraEfe <ara.fnz@live.com.ar>
2023-05-15 11:56:47 -0300

    MR-6: created and connected routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
-import superAdminRoutes from './super-admins';
+import trainerRoutes from './trainers';
 
 const router = express.Router();
 
-router.use('/super-admins', superAdminRoutes);
+router.use('/trainers', trainerRoutes);
 
 export default router;
