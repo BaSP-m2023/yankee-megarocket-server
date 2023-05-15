918532847bae35b072586156f5a1518626386ad5
Franco rossi <rossifranco748@gmail.com>
2023-05-15 19:08:30 -0300

    MR-2:created validation admin-post

---
+++ src/validations/admins.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+const validationCreation = (req, res, next) => {
+  const validationAdmin = Joi.object({
+    firstName: Joi.string().required().min(3).max(10),
+    lastName: Joi.string().required().min(3).max(10),
+    dni: Joi.number().required().min(1000000),
+    phone: Joi.number().required().min(1000000000).max(9999999999),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8).alphanum(),
+  });
+
+  const validation = validationAdmin.validate(req.body);
+  if (!validation.error) return next();
+  return res.status(400).json({
+    message: `There was an error: ${validation.error.details[0].message}`,
+    data: undefined,
+    error: true,
+  });
+};
+module.exports = {
+  validationCreation,
+};
