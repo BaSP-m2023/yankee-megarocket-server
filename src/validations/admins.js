d97f1f6b7ccb387282314043f60aec05daf79cf6
Roberto-Orazi <robervgg@gmail.com>
2023-05-15 11:07:32 -0300

    MR-3: Add admins Validations

--- src/validations/admins.js
+++ src/validations/admins.js
@@ -1,12 +1,46 @@
-import Joi from 'joi';
+const Joi = require('joi');
 
-const validateAdmin = Joi.object({
-  firstName: Joi.string().min.length(3).max.length(15).required(),
-  lastName: Joi.string().min.length(3).max.length(15).required(),
-  dni: Joi.string().min.length(7).max.length(8).required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().min.length(10).max.length(10).required(),
-  password: Joi.string().min.length(6).required(),
-});
+const validateCreateAdmin = (req, res, next) => {
+  const validateAdmin = Joi.object({
+    firstName: Joi.string().min.length(3).max.length(15).required(),
+    lastName: Joi.string().min.length(3).max.length(15).required(),
+    dni: Joi.string().min(10000000).max(99999999).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().min(1000000).max(9999999).required(),
+    password: Joi.string().min.length(6).required(),
+  });
+  const validation = validateAdmin.validate(req.body);
+  if (!validation.error) {
+    next();
+  }
+  res.status(400).json({
+    message: `There was an error: ${validation.error.details[0].message}`,
+    data: undefined,
+    error: true,
+  });
+};
 
-export default validateAdmin;
+const validateUpdateAdmin = (req, res, next) => {
+  const validateAdmin = Joi.object({
+    firstName: Joi.string().min.length(3).max.length(15),
+    lastName: Joi.string().min.length(3).max.length(15),
+    dni: Joi.string().min(10000000).max(99999999),
+    email: Joi.string().email(),
+    phone: Joi.string().min(100000000).max(999999999),
+    password: Joi.string().min.length(6),
+  });
+  const validation = validateAdmin.validate(req.body);
+  if (!validation.error) {
+    next();
+  }
+  req.status(400).json({
+    message: `Error: ${validation.error.details[0].message}`,
+    data: undefined,
+    error: true,
+  });
+};
+
+module.exports = {
+  validateCreateAdmin,
+  validateUpdateAdmin,
+};
