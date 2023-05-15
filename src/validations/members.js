9f023e551e152939b228eede0b47f2597a37ac97
JuanManuel-Blois <juan.m.blois@gmail.com>
2023-05-14 21:27:47 -0300

    MR-04: modified validations

--- src/validations/members.js
+++ src/validations/members.js
@@ -4,9 +4,9 @@ const memberCreateValidation = (req, res, next) => {
   const memberValidation = Joi.object({
     firstName: Joi.string().min(3).max(20).required(),
     lastName: Joi.string().min(3).max(20).required(),
-    dni: Joi.number().min(7).max(8).required(),
+    dni: Joi.number().min(1000000).max(99999999).required(),
     email: Joi.string().email().lowercase().required(),
-    phone: Joi.number().min(10).max(10).required(),
+    phone: Joi.number().min(1000000000).max(9999999999).required(),
     password: Joi.string().min(8).max(30).required(),
   });
 
