7f37bd445996dc983956d3d768a14e7ff7c2e693
AraEfe <ara.fnz@live.com.ar>
2023-05-15 19:16:48 -0300

    MR-14: updated postman collection, modified routes

--- src/routes/super-admins.js
+++ src/routes/super-admins.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
 import validateSuperAdmin from '../validations/super-admins';
-import { getSuperAdmins, getSuperAdmin } from '../controllers/super-admins';
+import { getSuperAdmins, getSuperAdmin, createSuperAdmin } from '../controllers/super-admins';
 
 const router = express.Router();
 
 router.get('/', getSuperAdmins);
 router.get('/:id', getSuperAdmin);
-router.post('/', validateSuperAdmin, getSuperAdmin);
+router.post('/', validateSuperAdmin, createSuperAdmin);
 
 export default router;
