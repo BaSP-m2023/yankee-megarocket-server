d4dba62228286fe7e6e2787683276e97a1be2e88
Ticiano Licarzze <ticilicarzze@gmail.com>
2023-05-16 14:06:36 -0300

    MR-10: Change name for consistency

--- src/routes/subscriptions.js
+++ src/routes/subscriptions.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { createSubscription, getAllSubscriptions, getSubscription } from '../controllers/subscriptions';
-import { validateSubCreation } from '../validations/subscriptions';
+import { validateSubscription } from '../validations/subscriptions';
 
 const router = express.Router();
 
 router.get('/', getAllSubscriptions);
 router.get('/:id', getSubscription);
-router.post('/', validateSubCreation, createSubscription);
+router.post('/', validateSubscription, createSubscription);
 
 export default router;
--- src/validations/subscriptions.js
+++ src/validations/subscriptions.js
@@ -1,13 +1,13 @@
 const Joi = require('joi');
 
-const validateSubCreation = (req, res, next) => {
-  const validateSubscription = Joi.object({
+const validateSubscription = (req, res, next) => {
+  const validateData = Joi.object({
     classId: Joi.string().max(24).required(),
     memberId: Joi.string().max(24).required(),
     date: Joi.date().greater('now').required(),
   });
 
-  const subscriptionData = validateSubscription.validate(req.body);
+  const subscriptionData = validateData.validate(req.body);
   if (!subscriptionData.error) return next();
   return res.status(400).json({
     message: `An error has occurred: ${subscriptionData.error.details[0].message}`,
@@ -16,4 +16,4 @@ const validateSubCreation = (req, res, next) => {
   });
 };
 
-module.exports = { validateSubCreation };
+module.exports = { validateSubscription };
