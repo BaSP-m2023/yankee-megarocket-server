7cfece071a549094d895595b697d69af0d0936cb
Roberto-Orazi <robervgg@gmail.com>
2023-05-16 21:08:46 -0300

    MR-3: Try to solve conflicts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 
-import superAdminRoutes from './super-admins';
+import classesRoutes from './classes';
+import membersRoutes from './members';
+import superAdminsRoutes from './super-admins';
+import trainerRoutes from './trainers';
 import adminsRoutes from './admins';
+
+import activityRoutes from './activities';
 
 const router = express.Router();
 
-router.use('/super-admins', superAdminRoutes);
+router.use('/classes', classesRoutes);
+router.use('/members', membersRoutes);
+router.use('/activities', activityRoutes);
+router.use('/super-admins', superAdminsRoutes);
+router.use('/trainers', trainerRoutes);
 router.use('/admins', adminsRoutes);
 
 export default router;
