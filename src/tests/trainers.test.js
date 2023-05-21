37542f8690f1f94ec1c747d6d476f2b41972ec0c
Ticiano Licarzze <ticilicarzze@gmail.com>
2023-05-21 18:23:36 -0300

    MR-35: get test for status 200 and 404

--- src/tests/trainers.test.js
+++ src/tests/trainers.test.js
@@ -1,5 +1,31 @@
-// Basic test to avoid conflicts
-// REMOVE THIS TEST AND MAKE YOURS!
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import Trainer from '../models/Trainer';
+import trainerSeed from '../seeds/trainers';
+
+// const mockTrainer = {
+//   firstName: 'King',
+//   lastName: 'James',
+//   dni: 22222222,
+//   email: 'kingjames@email.com',
+//   phone: 6563233000,
+//   password: 'lebron1010',
+//   rate: 15,
+//   assignedActivities: ['646939300b3782bf437c381b'],
+// };
+
+beforeAll(async () => {
+  await Trainer.collection.insertMany(trainerSeed);
+});
+
+describe('GET /api/trainers', () => {
+  test('Should return status 200', async () => {
+    const response = await request(app).get('/api/trainers').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('Should return status 404', async () => {
+    const response = await request(app).get('/api/trainer').send();
+    expect(response.status).toBe(404);
+  });
 });
