f2715e61fadd710be425e9b9e3cd20bee8fd5b01
JuanManuel-Blois <juan.m.blois@gmail.com>
2023-05-21 20:45:41 -0300

    MR-32: added delete and put test methods

--- src/tests/subscriptions.test.js
+++ src/tests/subscriptions.test.js
@@ -7,13 +7,29 @@ beforeAll(async () => {
   await Subscription.collection.insertMany(subSeed);
 });
 
-// const mockSubscription = {
-//   classId: '64693c420b3782bf437c382f',
-//   members: [
-//     '64655cf22c2d037df75e72dd', '64691789d00ee55da969d212',
-//   ],
-//   date: '2023-10-05T03:00:00.000Z',
-// };
+describe('PUT /api/subscriptions', () => {
+  test('should update one subscription', async () => {
+    const response = await request(app).put('/api/subscriptions/64694cb404b48dafdecfa44a').send({
+      classId: '64694cb404b48dafdecfa12e',
+      members: [
+        '64655cf22c2d037df75e72dd', '64691789d00ee55da969d212',
+      ],
+      date: '2023-10-05T03:00:00.000Z',
+    });
+    expect(response.status).toBe(200);
+  });
+
+  test('should send error 400 becasue this ID is invalid', async () => {
+    const response = await request(app).put('/api/subscriptions/64694cb404b48dafdecfa123').send({
+      classId: '64694cb404b48dafdecfa12e',
+      members: [
+        '64655cf22c2d037df75e72dd', '64691789d00ee55da969d212',
+      ],
+      date: '2023-10-05T03:00:00.000Z',
+    });
+    expect(response.status).toBe(400);
+  });
+});
 
 describe('DELETE /api/subscriptions', () => {
   test('should delete one subscription', async () => {
@@ -26,7 +42,3 @@ describe('DELETE /api/subscriptions', () => {
     expect(response.status).toBe(400);
   });
 });
-
-// describe ('PUT /api/subscriptions', () => {
-//   test('should update one subscription', async () => {})
-// })
