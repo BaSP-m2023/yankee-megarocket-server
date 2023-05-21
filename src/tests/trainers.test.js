3d57c0ff64d361997c610eed2edbcc21a4a67ef9
Ticiano Licarzze <ticilicarzze@gmail.com>
2023-05-21 18:54:33 -0300

    MR-35: getById and create testing

--- src/tests/trainers.test.js
+++ src/tests/trainers.test.js
@@ -3,16 +3,16 @@ import app from '../app';
 import Trainer from '../models/Trainer';
 import trainerSeed from '../seeds/trainers';
 
-// const mockTrainer = {
-//   firstName: 'King',
-//   lastName: 'James',
-//   dni: 22222222,
-//   email: 'kingjames@email.com',
-//   phone: 6563233000,
-//   password: 'lebron1010',
-//   rate: 15,
-//   assignedActivities: ['646939300b3782bf437c381b'],
-// };
+const mockTrainer = {
+  firstName: 'King',
+  lastName: 'James',
+  dni: 22222222,
+  email: 'kingjames@email.com',
+  phone: 6563233000,
+  password: 'lebron1010',
+  rate: 15,
+  assignedActivities: ['646939300b3782bf437c381b'],
+};
 
 beforeAll(async () => {
   await Trainer.collection.insertMany(trainerSeed);
@@ -25,7 +25,31 @@ describe('GET /api/trainers', () => {
   });
 
   test('Should return status 404', async () => {
-    const response = await request(app).get('/api/trainer').send();
+    const response = await request(app).get('/api/trainersadasd').send();
+    expect(response.status).toBe(404);
+  });
+});
+
+describe('GET /api/trainers/:id', () => {
+  test('Should return status 200', async () => {
+    const response = await request(app).get('/api/trainers/64693b1d0b3782bf437c3826').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('Should return status 404', async () => {
+    const response = await request(app).get('/api/trainer/64693b1d0b3782bf437c3821').send();
+    expect(response.status).toBe(404);
+  });
+});
+
+describe('POST /api/trainers/', () => {
+  test('Should return status 201', async () => {
+    const response = await request(app).post('/api/trainers').send(mockTrainer);
+    expect(response.status).toBe(201);
+  });
+
+  test('Should return status 404', async () => {
+    const response = await request(app).post('/api/trainer').send();
     expect(response.status).toBe(404);
   });
 });
