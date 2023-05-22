2cf24b06f87791699be946318eec86a07995106a
AraEfe <ara.fnz@live.com.ar>
2023-05-21 21:02:32 -0300

    MR-28: Created branch, added seeds and tests files for classes

---
+++ src/seeds/classes.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+export default [{
+  _id: new mongoose.Types.ObjectId('64693c420b3782bf437c382f'),
+  activityId: '646939300b3782bf437c381b',
+  hour: 14,
+  day: 'tuesday',
+  trainerId: '64693b1d0b3782bf437c3826',
+  maxCapacity: 10,
+}];
--- src/tests/classes.test.js
+++ src/tests/classes.test.js
@@ -1,5 +1,40 @@
-// Basic test to avoid conflicts
-// REMOVE THIS TEST AND MAKE YOURS!
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import Class from '../models/Class';
+import classesSeed from '../seeds/classes';
+
+const mockClass = {
+  activityId: '646939300b3782bf437c381b',
+  hour: 14,
+  day: 'tuesday',
+  trainerId: '64693b1d0b3782bf437c3826',
+  maxCapacity: 10,
+};
+
+beforeAll(async () => {
+  await Class.collection.insertMany(classesSeed);
+});
+
+describe('PUT /api/classes/:id', () => {
+  test('Should respond with a  200 status', async () => {
+    const response = await request(app).put('/api/classes/64693c420b3782bf437c382f').send(mockClass);
+    expect(response.status).toBe(200);
+  });
+
+  test('Should respond with a 400 status', async () => {
+    const response = await request(app).put('/api/classes/64693c420b3782bf437c382r').send(mockClass);
+    expect(response.status).toBe(400);
+  });
+});
+
+describe('DELETE /api/classes/:id', () => {
+  test('Should respond with a  200 status', async () => {
+    const response = await request(app).put('/api/classes/64693c420b3782bf437c382f').send(mockClass);
+    expect(response.status).toBe(200);
+  });
+
+  test('Should respond with a 400 status', async () => {
+    const response = await request(app).put('/api/classes/64693c420b3782bf437c3829').send(mockClass);
+    expect(response.status).toBe(400);
+  });
 });
