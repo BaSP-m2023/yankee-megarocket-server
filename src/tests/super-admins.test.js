8ac143f4c8550da827e629b6af8a93cceea3b813
Jazmin Presidente <jazmin.presi@gmail.com>
2023-05-21 22:33:40 -0300

    MR-33: Created tests for get and get by id

--- src/tests/super-admins.test.js
+++ src/tests/super-admins.test.js
@@ -1,5 +1,31 @@
-// Basic test to avoid conflicts
-// REMOVE THIS TEST AND MAKE YOURS!
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import SuperAdmin from '../models/SuperAdmin';
+import superAdminSeed from '../seeds/super-admins';
+
+const mockSuper = {
+  email: 'superadmin@super.com',
+  password: 'superadmin123',
+};
+
+beforeAll(async () => {
+  await SuperAdmin.collection.insertMany(superAdminSeed);
+});
+
+describe('get/api/super-admins', () => {
+  test('should return status 200', async () => {
+    const response = await request(app).get('/api/super-admins').send();
+    expect(response.status).toBe(200);
+  });
+  test('should return status 404', async () => {
+    const response = await request(app).get('/api/super-admin').send();
+    expect(response.status).toBe(404);
+  });
+});
+
+describe('get/api/super-admins/:id', () => {
+  test('should return status 200', async () => {
+    const response = await request(app).get('/api/super-admins').send();
+    expect(response.status).toBe(200);
+  });
 });
