6aacf8e2e371a0a90c4c74cf3a69db20458220f2
AleMac17 <alemacia17@gmail.com>
2023-05-21 23:45:48 -0300

    MR-29: add seeds and get, getById and post tests

--- src/tests/members.test.js
+++ src/tests/members.test.js
@@ -1,5 +1,125 @@
-// Basic test to avoid conflicts
-// REMOVE THIS TEST AND MAKE YOURS!
-test('should pass a basic test', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import app from '../app';
+import Member from '../models/Member';
+import memberSeed from '../seeds/members';
+
+beforeEach(async () => {
+  await Member.collection.insertMany(memberSeed);
+});
+
+afterEach(async () => {
+  await Member.collection.deleteMany();
+});
+
+const mockMemberSeed = {
+  id: '64691519333281ea60b8069d',
+  firstName: 'Juan',
+  lastName: 'Perez',
+  dni: 41317666,
+  email: 'testeando@Radium.com',
+  phone: 1763060997,
+  password: 'd9vUeOyuKs',
+};
+const mockMemberGood = {
+  firstName: 'Juan',
+  lastName: 'Perez',
+  dni: 41317666,
+  email: 'testeando@Radium.com',
+  phone: 1763060997,
+  password: 'd9vUeOyuKs',
+};
+const mockMemberEmpty = {
+};
+const mockMemberBadValidations = {
+  firstName: 23,
+  lastName: 'Perezaaaaaaaaaaaaaaaaaasssssssssssssssssssassadsasdaaaaaa',
+  dni: '41317666',
+  email: 'testeandoRadium.com',
+  phone: 176306099700000000,
+  password: 'd9vUeOyuKsasdasd@',
+};
+const mockIdBad = {
+  id: '64691519333281ea60b8069a',
+};
+
+describe('GET /api/members', () => {
+  test('should return status 200', async () => {
+    const response = await request(app).get('/api/members').send();
+    expect(response.status).toBe(200);
+    expect(response.body.data.length).toBe(2);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.message).toBe('Members found successfully!:');
+  });
+  test('should return status 404', async () => {
+    await Member.collection.deleteMany();
+    const response = await request(app).get('/api/members').send();
+    expect(response.body.error).toBeTruthy();
+    expect(response.status).toBe(404);
+    expect(response.body.data.length).toBe(0);
+    expect(response.body.message).toBe('There are no members!');
+  });
+});
+
+describe('GETById /api/members/:id', () => {
+  test('should return status 400, send no id', async () => {
+    const response = await request(app).get('/api/members/:id').send();
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBeTruthy();
+    expect(response.body.data.length).toBe(undefined);
+  });
+  test('should return status 400, send invalid id', async () => {
+    const response = await request(app).get(`/api/members/${mockMemberBadValidations.phone}`).send();
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBeTruthy();
+    expect(response.body.data.length).toBe(undefined);
+  });
+  test('should return status 404, send valid id but without member', async () => {
+    const response = await request(app).get(`/api/members/${mockIdBad.id}`).send();
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBeTruthy();
+    expect(response.body.data.length).toBe(undefined);
+  });
+  test('should return status 200', async () => {
+    const response = await request(app).get(`/api/members/${mockMemberSeed.id}`).send();
+    expect(response.status).toBe(200);
+    /* eslint no-underscore-dangle: 0 */
+    expect(response.body.data._id).toBe(mockMemberSeed.id);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.message).toBe('Member found successfully!');
+  });
+});
+
+describe('post /api/members', () => {
+  test('should return status 2010', async () => {
+    const response = await request(app)
+      .post('/api/members/')
+      .send(mockMemberGood);
+    expect(response.status).toBe(201);
+    expect(response.body.data.dni).toBe(mockMemberGood.dni);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.message).toBe('Member created successfully!');
+  });
+  test('should return status 400, send data empty', async () => {
+    const response = await request(app)
+      .post('/api/members')
+      .send(mockMemberEmpty);
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBe(undefined);
+  });
+  test('should return status 400, send data without a data', async () => {
+    const response = await request(app)
+      .post('/api/members');
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBe(undefined);
+  });
+  test('should return status 400, send data with bad inputs', async () => {
+    const response = await request(app)
+      .post('/api/members')
+      .send(mockMemberBadValidations);
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBe(undefined);
+  });
 });
