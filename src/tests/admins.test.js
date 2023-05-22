841285521445003b244fa91d04d555e71fd1db8a
Juan Sobral <JuanignacioSobral@gmail.com>
2023-05-22 13:16:38 -0300

    MR-23: Updated PR to adjust to comments given by reviewers and fixes mentioned on today's meeting

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -12,22 +12,46 @@ const mockAdmin = {
   password: 'Passu0rdg',
 };
 
-let mockAdminId;
+const mockAdminId = adminSeed[0]._id; // eslint-disable-line
+const mockAdminIdNotFound = '6466631c2c2d037df75e72f5';
 
-
-beforeAll(async () => {
+beforeEach(async () => {
   await Admin.collection.insertMany(adminSeed);
+});
+afterEach(async () => {
+  await Admin.collection.deleteMany();
 });
 
 describe('GET /api/admins', () => {
   test('should return status 200', async () => {
     const response = await request(app).get('/api/admins').send();
     expect(response.status).toBe(200);
-    expect(response.error).toBeFalsy();
   });
   test('should return status 404', async () => {
-    const response = await request(app).get('/api/admin').send();
+    await Admin.collection.deleteMany();
+    const response = await request(app).get('/api/admins').send();
     expect(response.status).toBe(404);
+  });
+  test('should return status 500', async () => {
+    jest.spyOn(Admin, 'find').mockRejectedValue(new Error('Somenthing went wrong with the server'));
+    const response = await request(app).get('/api/admins').send();
+    expect(response.status).toBe(500);
+  });
+});
+
+describe('GET BY ID /api/admins', () => {
+  test('should return status 200', async () => {
+    const response = await request(app).get(`/api/admins/${mockAdminId}`).send();
+    expect(response.status).toBe(200);
+  });
+  test('should return status 404', async () => {
+    const response = await request(app).get(`/api/admins/${mockAdminIdNotFound}`).send();
+    expect(response.status).toBe(404);
+  });
+  test('should return status 500', async () => {
+    jest.spyOn(Admin, 'findById').mockRejectedValue(new Error('Somenthing went wrong with the server'));
+    const response = await request(app).get(`/api/admins/${mockAdminId}`).send();
+    expect(response.status).toBe(500);
   });
 });
 
@@ -35,27 +59,14 @@ describe('POST /api/admins', () => {
   test('should create a new admin and return a status 201', async () => {
     const response = await request(app).post('/api/admins').send(mockAdmin);
     expect(response.status).toBe(201);
-    mockAdminId = response.body.data._id;
+  });
+  test('should return status 400', async () => {
+    const response = await request(app).post('/api/admins').send({});
+    expect(response.status).toBe(400);
+  });
+  test('should return status 500', async () => {
+    jest.spyOn(Admin, 'create').mockRejectedValue(new Error('Somenthing went wrong with the server'));
+    const response = await request(app).get('/api/admins').send(mockAdmin);
+    expect(response.status).toBe(500);
   });
 });
-
-describe('GET BY ID /api/admins', () => {
-  test('should return status 200', async () => {
-    const response = await request(app).get(`/api/admins/${ mockAdminId }`).send();
-    expect(response.status).toBe(200);
-    expect(response.error).toBeFalsy();
-  });
-  test('should return status 404', async () => {
-    const response = await request(app).get('/api/admin').send();
-    expect(response.status).toBe(404);
-  });
-});
-
-describe('DELETE /api/admins', () => {
-  test('should delete an existing admin and return a status 200', async () => {
-    const response = await request(app).delete(`/api/admins/${mockAdminId}`);
-    expect(response.status).toBe(200);
-  });
-});
-
-
