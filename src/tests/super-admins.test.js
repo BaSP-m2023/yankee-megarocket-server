7f8b4855f5c9a5e0de1b4a6c4fd0451416e92514
Matias Sauvanet <matiassauvanet@gmail.com>
2023-05-22 19:36:10 -0300

    MR-34: Finish Delete Tests

--- src/tests/super-admins.test.js
+++ src/tests/super-admins.test.js
@@ -1,9 +1,8 @@
-// Basic test to avoid conflicts
+/* eslint-disable no-underscore-dangle */
 import request from 'supertest';
 import app from '../app';
 import superAdmin from '../models/SuperAdmin';
 import adminSeed from '../seeds/super-admins';
-import SuperAdmin from '../models/SuperAdmin';
 
 beforeEach(async () => {
   await superAdmin.collection.insertMany(adminSeed);
@@ -16,9 +15,7 @@ afterEach(async () => {
 
 const superAdminId = adminSeed[0]._id;
 
-const invalidSuperAdmin = '6561-361#0ebcc@7a2a1b9f'
-
-const notFoundId = '6466b3620ebbac7a2a1b7f8f'
+const notFoundId = '6466b3620ebbac7a2a1b7f8f';
 
 const missingEmailFieldSuperAdmin = {
   _id: '6466b3610ebbac7a2a1b9f8f',
@@ -35,32 +32,49 @@ describe('PUT /api/super-admin', () => {
     const response = await request(app).put(`/api/super-admins/${superAdminId}`).send(putTestSuperAdmin);
     expect(response.status).toBe(200);
     expect(response.body.data).toBeDefined();
-    expect(response.body.message).toBe('Super Admin Updated Successfully!')
-    expect(response.body.error).toBe(false);
+    expect(response.body.message).toBe('Super Admin Updated Successfully!');
+    expect(response.body.error).toBeFalsy();
   });
   test('should not found id, return error 404', async () => {
     const response = await request(app).put(`/api/super-admins/${notFoundId}`).send(putTestSuperAdmin);
     expect(response.status).toBe(404);
     expect(response.body.data).toBeDefined();
-    expect(response.body.message).toBe('Super Admin could not be Found and updated')
-    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBe('Super Admin could not be Found and updated');
+    expect(response.body.error).toBeTruthy();
   });
-  test('missing email, return 400', async () => {
+  test('missing email, return error 400', async () => {
     const response = await request(app).put(`/api/super-admins/${superAdminId}`).send(missingEmailFieldSuperAdmin);
     expect(response.status).toBe(400);
     expect(response.body.data).toBeUndefined();
-    expect(response.body.message).toBe('There was an error: \"email\" is required');
-    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBe('There was an error: "email" is required');
+    expect(response.body.error).toBeTruthy();
   });
-  test('Should respond server error, return 500', async () => {
-    jest.spyOn(SuperAdmin, 'findByIdAndUpdate').mockRejectedValue(new Error('Something went wrong'));
+  test('Should respond server error, return status 500', async () => {
+    jest.spyOn(superAdmin, 'findByIdAndUpdate').mockRejectedValue(new Error('Something went wrong'));
     const response = await request(app).put(`/api/super-admins/${superAdminId}`).send(putTestSuperAdmin);
     expect(response.status).toBe(500);
   });
 });
 
-// describe('DELETE /api/super-admin', () => {
-//   test('Should delete one super admin', async () => {
-
-//   });
-// });
+describe('DELETE /api/super-admin', () => {
+  test('Should delete one super admins, return status 200', async () => {
+    const response = await request(app).delete(`/api/super-admins/${superAdminId}`).send();
+    expect(response.status).toBe(200);
+    expect(response.body.data).toBeDefined();
+    expect(response.body.message).toBe('Super Admin Deleted Successfully!');
+    expect(response.body.error).toBeFalsy();
+  });
+  test('Should not be found and delete an super admin, return error 400', async () => {
+    await superAdmin.deleteMany();
+    const response = await request(app).delete(`/api/super-admins/${superAdminId}`).send();
+    expect(response.status).toBe(400);
+    expect(response.body.data).toEqual({});
+    expect(response.body.message).toBe('Super Admin could not be found and deleted!');
+    expect(response.body.error).toBeTruthy();
+  });
+  test('Should respond server error, return status 500', async () => {
+    jest.spyOn(superAdmin, 'findByIdAndDelete').mockRejectedValue(new Error('Something went wrong'));
+    const response = await request(app).delete(`/api/super-admins/${superAdminId}`).send();
+    expect(response.status).toBe(500);
+  });
+});
