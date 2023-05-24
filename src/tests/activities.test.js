6cf754e07db544211d5bff829f7db8b26315b5f7
Roberto-Orazi <robervgg@gmail.com>
2023-05-23 21:26:21 -0300

    BaSP-MR-26: Corrections

--- src/tests/activities.test.js
+++ src/tests/activities.test.js
@@ -1,13 +1,13 @@
-/* eslint-disable no-underscore-dangle */
 import request from 'supertest';
 import app from '../app';
 import Activity from '../models/Activity';
 import activitySeed from '../seeds/activities';
-
+/* eslint-disable no-underscore-dangle */
 const activityId = activitySeed[0]._id;
+
 const mockActivity = {
-  activity: 'Crossfit',
-  description: 'Made from the best of the best',
+  activityName: 'Circuit Training',
+  activityDescription: 'A high-intensity workout.',
 };
 
 beforeEach(async () => {
@@ -15,6 +15,7 @@ beforeEach(async () => {
 });
 afterEach(async () => {
   await Activity.collection.deleteMany();
+  jest.restoreAllMocks();
 });
 
 describe('PUT /api/activities/:id', () => {
@@ -23,21 +24,21 @@ describe('PUT /api/activities/:id', () => {
     expect(response.status).toBe(200);
     expect(response.body.error).toBeFalsy();
     expect(response.body.data).toBeDefined();
-    expect(response.body.message).toBe('Activity has been updated successfully');
+    expect(response.body.message).toBe('Activity was updated successfully!');
   });
   test('It should send error 400 because the Id does not exist', async () => {
     const response = await request(app).put(`/api/activities/${activityId}`).send({});
     expect(response.status).toBe(400);
     expect(response.body.error).toBeTruthy();
-    expect(response.body.data).toBeDefined();
-    expect(response.body.message).toBe('Activity could not been found');
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toBe('There was an error in the validation: "activityName" is required');
   });
   test('It should send error 400 because the Id is invalid', async () => {
     const response = await request(app).put(`/api/activities/${activityId}`).send();
     expect(response.status).toBe(400);
     expect(response.body.error).toBeTruthy();
-    expect(response.body.data).toBeDefined();
-    expect(response.body.message).toBe('Activity could not been found');
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toBe('There was an error in the validation: "activityName" is required');
   });
   test('It should send error 500', async () => {
     jest.spyOn(Activity, 'findByIdAndUpdate').mockRejectedValue(new Error('Something went wrong'));
@@ -46,21 +47,21 @@ describe('PUT /api/activities/:id', () => {
     expect(response.body.data).toBe(undefined);
   });
 });
-
 describe('DELETE /api/activities/:id', () => {
   test('It should delete one activity', async () => {
     const response = await request(app).delete(`/api/activities/${activityId}`).send();
     expect(response.status).toBe(200);
     expect(response.body.error).toBeFalsy();
     expect(response.body.data).toBeDefined();
-    expect(response.body.message).toBe('Activity has been deleted successfully');
+    expect(response.body.message).toBe('Activity was deleted successfully!');
   });
   test('It should send error 400 because the Id does not exist', async () => {
+    await Activity.deleteMany();
     const response = await request(app).delete(`/api/activities/${activityId}`).send();
     expect(response.status).toBe(400);
     expect(response.body.error).toBeTruthy();
     expect(response.body.data).toBeDefined();
-    expect(response.body.message).toBe('Activity could not been found');
+    expect(response.body.message).toBe('Activity could not be found and Deleted!');
   });
   test('It should send error 500', async () => {
     jest.spyOn(Activity, 'findByIdAndDelete').mockRejectedValue(new Error('Something went wrong'));
